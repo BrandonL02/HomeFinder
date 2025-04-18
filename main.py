1ce05273221ad8fdd4c3a8dd79d7010e9bb265a3
Brandon Latimer <blatim9@wgu.edu>
2025-04-17 23:15:08 -0400

    adds apartment search criteria

--- main.py
+++ main.py
@@ -3,19 +3,51 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+# Access apartments.com
 
 driver = webdriver.Chrome()
 driver.get('https://www.apartments.com')
-time.sleep(3)
+
+time.sleep(3)  # wait for page to load
+
+# Search city in the searchbar
+
 search_box = driver.find_element(By.XPATH, '//*[@id="quickSearchLookup"]')
-search_box.send_keys('Tampa, FL',Keys.RETURN)
+search_box.send_keys('Tampa, FL', Keys.RETURN)
 
+time.sleep(3)  # wait for page to load
 
-time.sleep(3)
+# Set apartment criteria
 
-search_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/section[1]/div[1]/section/div/fieldset/div/div/button[2]')
-search_button.click()
+# Price
 
-time.sleep(10) # Let the user actually see something!
+driver.find_element(By.XPATH, '//*[@id="rentRangeLink"]').click()
 
-#driver.quit()
+min_price = driver.find_element(By.XPATH, '//*[@id="min-input"]')
+min_price.send_keys('1000')
+
+max_price = driver.find_element(By.XPATH, '//*[@id="max-input"]')
+max_price.send_keys('1300')
+
+time.sleep(2)
+
+# Beds/Baths
+
+driver.find_element(By.XPATH, '//*[@id="bedRangeLink"]').click()
+
+for x in range(1, 5):
+    if x != 2 and x != 3:
+        continue
+    # 2 = studio / 3 = one bedroom
+    driver.find_element(By.XPATH,
+                        f'/html/body/div[1]/main/section/header/div/span[3]/div/div/div/div[1]/div/button[{x}]').click()
+
+time.sleep(2)
+
+# Home type
+
+driver.find_element(By.XPATH, '//*[@id="typeSelect"]')
+
+driver.find_element(By.XPATH, '/html/body/div[1]/main/section/header/div/span[5]/div/div/div[1]/button[1]/i').click()
+
+time.sleep(5)
