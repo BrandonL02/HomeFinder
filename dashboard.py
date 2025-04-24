109267079e2074d4b166ebfeadbead6e7ce83eac
Brandon Latimer <blatim9@wgu.edu>
2025-04-24 19:25:47 -0400

    adds title to gui

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write(""" Welcome """)
+app_title = 'Tampa Apartment Rent Analysis'
+
+app_sub_title = 'Apartments.com data 2025'
+
+def main():
+    st.set_page_config(app_title)
+    st.title(app_title)
+
+if __name__ == "__main__":
+    main()
