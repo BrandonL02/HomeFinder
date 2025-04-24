b4d3c114b2eca6a89f6f8f1dc682a25122905f72
Brandon Latimer <blatim9@wgu.edu>
2025-04-24 19:58:20 -0400

    adds subtitle to gui

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,7 @@ app_sub_title = 'Apartments.com data 2025'
 def main():
     st.set_page_config(app_title)
     st.title(app_title)
+    st.caption(app_sub_title)
 
 if __name__ == "__main__":
     main()
