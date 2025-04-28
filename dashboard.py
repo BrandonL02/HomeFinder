a7cfcd435e5d138ce7908a36eae21ef7ccfcf508
Brandon Latimer <blatim9@wgu.edu>
2025-04-28 15:52:02 -0400

    imports db data to gui

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 app_title = 'Tampa Apartment Rent Analysis'
 
@@ -9,6 +10,16 @@ def main():
     st.set_page_config(app_title)
     st.title(app_title)
     st.caption(app_sub_title)
+
+    # Load data from the DB
+    conn = sqlite3.connect('apartment_data.db')
+    query = "SELECT * FROM apartments"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+
+    st.write(df.shape)
+    st.write(df.head())
+    st.write(df.columns)
 
 
 if __name__ == "__main__":
