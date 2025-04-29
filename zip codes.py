74f9ce0496505070d57bcaaaaac6f945e63fbdc6
Brandon Latimer <89661981+BrandonL02@users.noreply.github.com>
2025-04-28 21:56:11 -0400

    Update zip codes.py

--- zip codes.py
+++ zip codes.py
@@ -4,9 +4,6 @@ import re
 conn = sqlite3.connect('apartment_data.db')
 cursor = conn.cursor()
 
-places = ['8800 Boardwalk Trail Dr, Tampa, FL 33637', '2633 Azalea Garden Pl, Tampa, FL 33619',
-          '1250 E Madison St, Tampa, FL 33602', '10881 Caladesi Ave, Tampa, FL 33610']
-
 
 def get_zip(address):
     zip_code = re.findall('FL 3....', address)[0]
