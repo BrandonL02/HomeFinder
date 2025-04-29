a1d6279bf9c6ee08bb915abc2de501afcba22689
Brandon Latimer <blatim9@wgu.edu>
2025-04-28 21:00:57 -0400

    adds visual for avg bed count vs avg rent

--- visualizations.py
+++ visualizations.py
@@ -1,3 +1,4 @@
+import re
 import time
 import sqlite3
 import pandas as pd
@@ -5,10 +6,7 @@ import folium
 import altair as alt
 
 
-
-
 def zip_price_map():
-
     # Load data from the DB
     conn = sqlite3.connect('apartment_data.db')
     query = "SELECT ZipCode, MinPrice, MaxPrice FROM apartments"
@@ -48,8 +46,8 @@ def zip_price_map():
 
     return m
 
-def rent_histogram():
 
+def rent_histogram():
     # Load data from the DB
     conn = sqlite3.connect('apartment_data.db')
     query = "SELECT MinPrice, MaxPrice FROM apartments"
@@ -63,3 +61,58 @@ def rent_histogram():
     ).properties(title='Distribution of Tampa Apartment Rent Prices')
 
     return hist
+
+
+def bed_vs_price():
+    # Load data from the DB
+    conn = sqlite3.connect('apartment_data.db')
+    query = "SELECT * FROM apartments"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+
+    # Delete any rows with nulls
+    df = df.dropna()
+
+    # Get average bed count from string
+
+    pattern = r'\b(\w+|\d+)(?:\s*-\s*(\d+))?\s*Beds?\b'
+
+    for index, row in df.iterrows():
+
+        row_text = str(row.values)
+        match = re.search(pattern, row_text)
+
+        if match:
+            x = match.group(1)
+            y = int(match.group(2)) if match.group(2) else 0
+
+            if x == 'Studio':
+                x = 0
+            else:
+                x = int(x)
+
+            avg_bed_count = (x * y) / 2
+            df.loc[index, 'AvgBeds'] = avg_bed_count
+
+    # Calculate average apartment price
+
+    df['MeanPrice'] = (df['MaxPrice'] + df['MinPrice']) / 2
+
+    # Create scatter plot of mean price and average bed count
+
+    scatter = alt.Chart(df).mark_circle(size=60).encode(
+        x=alt.X('AvgBeds:Q', title='Average Bedroom Count'),
+        y=alt.Y('MeanPrice:Q', title='Average Rent Price ($)'),
+        tooltip=['ApartmentName', 'Address', 'MeanPrice', 'AvgBeds']
+    ).properties(
+        title='Average Rent vs. Average Bedroom Count'
+    )
+
+    st.altair_chart(scatter, use_container_width=True)
+
+    return df
+
+
+test = bed_vs_price()
+
+print(test)
