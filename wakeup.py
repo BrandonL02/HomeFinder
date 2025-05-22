72697cee9f9bc6fbb664a582b4bdb9b801d1d04a
Brandon Latimer <89661981+BrandonL02@users.noreply.github.com>
2025-05-21 22:47:58 -0400

    Update wakeup.py

--- wakeup.py
+++ wakeup.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from streamlit_app import STREAMLIT_APPS
+STREAMLIT_APPS = ['https://homefinder-tampa.streamlit.app/']
 import datetime
 
 # Set up Selenium webdriver
